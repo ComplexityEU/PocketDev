344a084cefe6e0383c656cf142decff8445136c9
Duo Incure <duoincuredev@gmail.com>
2023-02-23 23:00:55 +0000

    Update
    Updated to use `update.pmmp.io`, along with some extra messages to make the process more user friendly.
    Added the ability to choose between stable and alpha builds.

--- UpdatePMMP.php
+++ UpdatePMMP.php
@@ -1,13 +1,18 @@
 <?php
 declare(strict_types=1);
 
-echo("    - Updating PocketMine-MP -\n");
-
+$channel = "stable";
+if($argc === 2 && strtolower($argv[1]) === "-alpha") {
+    $channel = "alpha";
+}
 @mkdir(__DIR__ . "/server");
 if(file_exists(getcwd() . "/server/PocketMine-MP.phar")){
     unlink(getcwd() . "/server/PocketMine-MP.phar");
 }
-if(!copy("https://github.com/pmmp/PocketMine-MP/releases/latest/download/PocketMine-MP.phar", getcwd() . "/server/PocketMine-MP.phar")){
+
+$contents = json_decode(file_get_contents("https://update.pmmp.io/api?channel={$channel}"), true);
+echo("    - Found PocketMine-MP {$contents["base_version"]} for MCPE v{$contents["mcpe_version"]} -\n");
+if(!copy($contents["download_url"], getcwd() . "/server/PocketMine-MP.phar")){
     throw new \RuntimeException("Failed to download PocketMine-MP.phar");
 }
 echo("    - PocketMine-MP Update Completed -\n");
